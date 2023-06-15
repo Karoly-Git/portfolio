48aecba34217631a304c56324b4b39ee11320c7c
Karoly <hornyal.mobile@gmail.com>
2023-06-15 11:15:28 +0100

    CV update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { config } from './js/config'
 import Maintenance from './components/Maintenance_Section'
 import Website from './components/Website'
 
 /*
-- If isMaintenanceMode mode is true in config.js, then it returns Maintenance.jsx, which is a "maintenance in progress" message.
-- Othervise, it returns the Website.jsx, which is a single page portfolio with multiple sections.
+- If isMaintenanceMode mode is true in config.js, then it returns Maintenance.jsx
+- Othervise, it returns the Website.jsx
 */
 
 export default function App() {
