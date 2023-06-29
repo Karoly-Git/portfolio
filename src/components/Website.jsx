0748c150671c3cc7909a27a660ec089c06a5aa11
Karoly <hornyal.mobile@gmail.com>
2023-06-29 13:18:38 +0100

    Replacing BrowserRouter to HashRouter

--- src/components/Website.jsx
+++ src/components/Website.jsx
@@ -1,5 +1,5 @@
 import { React, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import ProjectDetails from './Page_Project_Details'
 import GetInTouch from './Section_Get_In_Touch'
